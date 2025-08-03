b3a186ec8d531dfa03e091966ba755815f9ed3c0
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-08-03 18:14:09 +0000

    No description

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer() {
+    const correctAnswer = 4;
+    let userAnswer = document.querySelector("input[name='quiz']:checked").value;
+    let feedback = document.getElementById("feedback");
+
+    if (userAnswer == correctAnswer){
+        feedback.textContent = "Correct! Well done.";
+    }
+    else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+
+let submitAnswer = document.getElementById("submit-answer");
+submitAnswer.addEventListener("click", checkAnswer);
